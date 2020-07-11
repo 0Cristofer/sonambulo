70e98a06f9f5273505518965b655dc6219bed236
Eduardo Felipe Ribeiro <edu.felipe1@outlook.com>
2020-07-11 16:31:26 -0300

    Cena teste criada, scripts de movimento

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody EnemyRigidbody;
+
+    [SerializeField] 
+    private float moveSpeed;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        EnemyRigidbody.velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * moveSpeed;
+    }
+}
