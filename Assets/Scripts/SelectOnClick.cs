d07ef895d8e47d5dd5db1993ea1f39f9e6f7bb10
Eduardo Felipe Ribeiro <edu.felipe1@outlook.com>
2020-07-12 01:18:17 -0300

    selecionar objeto, highlightar objeto que o mouse está passando em cima

---
+++ Assets/Scripts/SelectOnClick.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectOnClick : MonoBehaviour
+{
+    [SerializeField]
+    private Camera MainCamera;
+    
+    [SerializeField]
+    private GameObject SelectedObject;
+
+    [SerializeField]
+    private GameObject HighlightedObject;
+
+    [SerializeField]
+    private LayerMask SelectableLayer;
+
+    private Ray Ray;
+
+    private RaycastHit HitData;
+    
+    private void Update()
+    {
+        Ray = MainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(Ray, out HitData, 1000000, SelectableLayer))
+        {
+            HighlightedObject = HitData.transform.gameObject;
+            
+            if (Input.GetMouseButtonDown(0))
+            {
+                SelectedObject = HitData.transform.gameObject;
+            }
+            
+        }
+        else
+        {
+            HighlightedObject = null;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                SelectedObject = null;
+            }
+            
+        }
+    }
+}
