3741ee5e597893076aa859106dfff9b84bc2708f
Cristofer Oswald <cristoferoswald@gmail.com>
2020-07-12 14:30:44 -0300

    Corrigindo lever e hazard

--- Assets/Scripts/HazardController.cs
+++ Assets/Scripts/HazardController.cs
@@ -7,9 +7,7 @@ public class HazardController : MonoBehaviour, ILeverListener
 	private LeverController LeverController = null;
 	[SerializeField]
 	private Collider Collider = null;
-	[SerializeField]
-	private Collider VoidCollider = null;
-	
+
 	private void Start()
 	{
 		LeverController.AddLeverListener(this);
@@ -21,11 +19,9 @@ public class HazardController : MonoBehaviour, ILeverListener
 		{
 			case LeverState.On:
 				Collider.enabled = false;
-				VoidCollider.enabled = false;
 				break;
 			case LeverState.Off:
 				Collider.enabled = true;
-				VoidCollider.enabled = true;
 				break;
 			default:
 				throw new ArgumentOutOfRangeException(nameof(state), state, null);
