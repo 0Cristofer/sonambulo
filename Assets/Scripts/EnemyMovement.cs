c3562f688237d9d92d5bd7f119054c328c847a9e
Cristofer Oswald <cristoferoswald@gmail.com>
2020-07-11 22:15:14 -0300

    Corrigido animação do rato

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,15 +1,50 @@
 ﻿using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
+public abstract class EnemyMovement : MonoBehaviour
 {
     [SerializeField]
     private Rigidbody EnemyRigidbody = null;
-
     [SerializeField]
     private float MoveSpeed = 0f;
-    
-    private void Update()
+    [SerializeField]
+    private Vector3 ForwardDirection;
+    [SerializeField]
+    protected Vector3 BackwardsDirection;
+    [SerializeField]
+    private MovementDirection InitialDirection;
+
+    protected MovementDirection Direction { get; private set; }
+
+    private void Awake()
     {
-        EnemyRigidbody.velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * MoveSpeed;
+        Direction = InitialDirection;
+    }
+    
+    private void FixedUpdate()
+    {
+        EnemyRigidbody.velocity =
+            (Direction == MovementDirection.Forward ? ForwardDirection : BackwardsDirection) * MoveSpeed;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!other.gameObject.CompareTag("Bounceable"))
+            return;
+        
+        ChangeDirection();
+    }
+
+    private void ChangeDirection()
+    {
+        Direction = Direction == MovementDirection.Forward ? MovementDirection.Backwards : MovementDirection.Forward;
+        OnDirectionChanged();
+    }
+
+    protected abstract void OnDirectionChanged();
+
+    protected enum MovementDirection
+    {
+        Forward = 0,
+        Backwards
     }
 }
