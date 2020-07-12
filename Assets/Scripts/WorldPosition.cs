c3c1773069edb5f86c2f767f9419bceb06f32e62
Eduardo Felipe Ribeiro <edu.felipe1@outlook.com>
2020-07-12 01:52:23 -0300

    worldPosition

---
+++ Assets/Scripts/WorldPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+    private Camera MainCamera;
+    
+    [SerializeField]
+    private Vector3 WorldPosition;
+
+    [SerializeField]
+    private LayerMask FloorLayerMask;
+    
+    void Update()
+    {
+        Ray ray = MainCamera.ScreenPointToRay(Input.mousePosition);
+        
+        if (Physics.Raycast(ray, out var hitData, 100000, FloorLayerMask))
+        {
+            WorldPosition = hitData.point;
+        }
+    }
+}
